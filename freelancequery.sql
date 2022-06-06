8a1752aea1192894c02f569dddca78e51d66125d
hoang bao <baohoang.7882@hotmail.com>
2022-06-06 14:42:25 +0700

    Changed SQL script

--- freelancequery.sql
+++ freelancequery.sql
@@ -1,5 +1,5 @@
-CREATE DATABASE FreelancePlatform
-USE FreelancePlatform
+CREATE DATABASE FreelancePlatformBaoHLG
+USE FreelancePlatformBaoHLG
 
 --Table for User Account entity
 CREATE TABLE UserAccount (
@@ -17,7 +17,7 @@ CREATE TABLE UserAccount (
 --Table for Freelancer Account
 CREATE TABLE FreelancerAccount (
     ID int IDENTITY(1,1) PRIMARY KEY,
-    UserID int NOT NULL FOREIGN KEY REFERENCES UserAccount(ID),
+    UserID int NOT NULL,
 	Overview nvarchar(128),
 );
 
@@ -31,8 +31,8 @@ CREATE TABLE Company (
 --Table for Customer Account
 CREATE TABLE CustomerAccount (
     ID int IDENTITY(1,1) PRIMARY KEY,
-    UserID int NOT NULL FOREIGN KEY REFERENCES UserAccount(ID),
-	CompanyID int FOREIGN KEY REFERENCES Company(ID),
+    UserID int NOT NULL ,
+	CompanyID int ,
 );
 
 --Table for field
@@ -47,35 +47,35 @@ CREATE TABLE Tech (
     ID int IDENTITY(1,1) PRIMARY KEY,
     Name nvarchar(64) NOT NULL,
 	Description nvarchar(128) NOT NULL,
-	FieldID int FOREIGN KEY REFERENCES Field(ID),
+	FieldID int ,
 );
 
 --Table for Skill (Freelancer N-N Tech)
 CREATE TABLE Skill (
     ID int IDENTITY(1,1) PRIMARY KEY,
-	FreelancerID int FOREIGN KEY REFERENCES FreelancerAccount(ID),
-	TechID int FOREIGN KEY REFERENCES Tech(ID),
+	FreelancerID int ,
+	TechID int ,
 );
 
 --Table for Project (job post)
 CREATE TABLE Project (
     ID int IDENTITY(1,1) PRIMARY KEY,
-	CustomerID int FOREIGN KEY REFERENCES CustomerAccount(ID),
+	CustomerID int ,
 	Title nvarchar(64) NOT NULL,
 	Description nvarchar(128) NOT NULL,
 	Duration nvarchar(64) NOT NULL,
 	Scope nvarchar(64) NOT NULL,
 	HrsPerWeek int NOT NULL,
 	MaximumBudget int NOT NULL,
-	FieldID int NOT NULL FOREIGN KEY REFERENCES Field(ID),
+	FieldID int NOT NULL ,
 	Status int NOT NULL,
 );
 
 --Table for Tag (Tech N-N Project)
 CREATE TABLE Tag (
     ID int IDENTITY(1,1) PRIMARY KEY,
-	FreelancerID int FOREIGN KEY REFERENCES FreelancerAccount(ID),
-	TechID int FOREIGN KEY REFERENCES Tech(ID),
+	FreelancerID int ,
+	TechID int ,
 );
 
 --Table for apply status (dict)
@@ -88,19 +88,19 @@ CREATE TABLE ApplyStatus (
 --Table for Apply (proposal for project)
 CREATE TABLE Apply (
     ID int IDENTITY(1,1) PRIMARY KEY,
-	FreelancerID int FOREIGN KEY REFERENCES FreelancerAccount(ID),
-	ProjectID int FOREIGN KEY REFERENCES Project(ID),
+	FreelancerID int ,
+	ProjectID int ,
 	Date DateTime NOT NULL,
 	Bid int NOT NULL,
-	ApplyStatusID int FOREIGN KEY REFERENCES ApplyStatus(ID),
+	ApplyStatusID int ,
 );
 
 --Table for Report
 CREATE TABLE Report (
     ID int IDENTITY(1,1) PRIMARY KEY,
-	FreelancerID int FOREIGN KEY REFERENCES FreelancerAccount(ID),
-	ApplyID int FOREIGN KEY REFERENCES Apply(ID),
-	CustomerID int FOREIGN KEY REFERENCES CustomerAccount(ID),
+	FreelancerID int ,
+	ApplyID int ,
+	CustomerID int ,
 	Title nvarchar(64) NOT NULL,
 	Date DateTime NOT NULL,
 	ReportItem nvarchar(128) NOT NULL,
@@ -110,7 +110,7 @@ CREATE TABLE Report (
 --Table for Payment
 CREATE TABLE Payment (
     ID int IDENTITY(1,1) PRIMARY KEY,
-	ApplyID int FOREIGN KEY REFERENCES Apply(ID),
+	ApplyID int ,
 	Date DateTime NOT NULL,
 	PaymentMethod nvarchar(64) NOT NULL,
 );
@@ -118,9 +118,30 @@ CREATE TABLE Payment (
 --Table for Transaction
 CREATE TABLE Transactions (
     ID int IDENTITY(1,1) PRIMARY KEY,
-	PaymentID int FOREIGN KEY REFERENCES Payment(ID),
-	FreelancerID int FOREIGN KEY REFERENCES FreelancerAccount(ID),
+	PaymentID int ,
+	FreelancerID int ,
 	Amount int NOT NULL,
 );
 
-DROP DATABASE FreelancePlatform
+--Foreign keys
+ALTER TABLE FreelancerAccount ADD FOREIGN KEY(UserID) REFERENCES UserAccount(ID)
+ALTER TABLE CustomerAccount ADD FOREIGN KEY(UserID) REFERENCES UserAccount(ID)
+ALTER TABLE CustomerAccount ADD FOREIGN KEY(CompanyID) REFERENCES Company(ID)
+ALTER TABLE Tech ADD FOREIGN KEY(FieldID) REFERENCES Field(ID)
+ALTER TABLE Skill ADD FOREIGN KEY(FreelancerID) REFERENCES FreelancerAccount(ID)
+ALTER TABLE Skill ADD FOREIGN KEY(TechID) REFERENCES Tech(ID)
+ALTER TABLE Project ADD FOREIGN KEY(CustomerID) REFERENCES CustomerAccount(ID)
+ALTER TABLE Project ADD FOREIGN KEY(FieldID) REFERENCES Field(ID)
+ALTER TABLE Tag ADD FOREIGN KEY(FreelancerID) REFERENCES FreelancerAccount(ID)
+ALTER TABLE Tag ADD FOREIGN KEY(TechID) REFERENCES Tech(ID)
+ALTER TABLE Apply ADD FOREIGN KEY(FreelancerID) REFERENCES FreelancerAccount(ID)
+ALTER TABLE Apply ADD FOREIGN KEY(ProjectID) REFERENCES Project(ID)
+ALTER TABLE Apply ADD FOREIGN KEY(ApplyStatusID) REFERENCES ApplyStatus(ID)
+ALTER TABLE Report ADD FOREIGN KEY(FreelancerID) REFERENCES FreelancerAccount(ID)
+ALTER TABLE Report ADD FOREIGN KEY(ApplyID) REFERENCES Apply(ID)
+ALTER TABLE Report ADD FOREIGN KEY(CustomerID) REFERENCES CustomerAccount(ID)
+ALTER TABLE Payment ADD FOREIGN KEY(ApplyID) REFERENCES Apply(ID)
+ALTER TABLE Transactions ADD FOREIGN KEY(PaymentID) REFERENCES Payment(ID)
+ALTER TABLE Transactions ADD FOREIGN KEY(FreelancerID) REFERENCES FreelancerAccount(ID)
+
+DROP DATABASE FreelancePlatformBaoHLG

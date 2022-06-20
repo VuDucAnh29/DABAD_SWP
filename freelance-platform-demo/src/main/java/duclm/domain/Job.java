677ca50c984a712e60ed8606f3bd51e6a8aaeef7
Le Duc <92800540+leduc212@users.noreply.github.com>
2022-06-20 11:44:05 +0700

    push new code
    customer login/view all jobs/view own jobs/edit own jobs/delete and add jobs

---
+++ freelance-platform-demo/src/main/java/duclm/domain/Job.java
@@ -0,0 +1,65 @@
+package duclm.domain;
+
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.JoinColumn;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "jobs")
+public class Job implements Serializable {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long jobId;
+	
+	@Column(columnDefinition = "nvarchar(100) not null")
+	private String title;
+	
+	@Column(columnDefinition = "nvarchar(20) not null")
+	private String scope;
+	
+	@Column(columnDefinition = "nvarchar(30) not null")
+	private String duration;
+	
+	@Column(nullable = false)
+	private double budget;
+	
+	@Column(columnDefinition = "nvarchar(500) default 'Write a description for your job post...'")
+	private String description;
+
+	@Column(nullable = false)
+	private int status;
+	
+	@ManyToOne
+    @JoinColumn(name = "user_id")
+	User user;
+	
+	@ManyToOne
+    @JoinColumn(name = "field_id")
+	Field field;
+	
+	@ManyToMany
+	@JoinTable(
+	        name = "Tag", 
+	        joinColumns = { @JoinColumn(name = "job_id") }, 
+	        inverseJoinColumns = { @JoinColumn(name = "tech_id") }
+	    )
+	Set<Tech> techs = new HashSet<>();
+}

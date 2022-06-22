00460f9f23728b4d3a5c099a56fb80110f973a36
Le Duc <92800540+leduc212@users.noreply.github.com>
2022-06-22 09:26:52 +0700

    freelancer account

---
+++ freelance-platform-demo/src/main/java/duclm/controller/JobController.java
@@ -0,0 +1,120 @@
+package duclm.controller;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.UUID;
+
+import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import duclm.domain.Field;
+import duclm.domain.Job;
+import duclm.domain.User;
+import duclm.model.FieldDto;
+import duclm.model.JobDto;
+import duclm.model.TechDto;
+import duclm.model.UserDto;
+import duclm.repository.FieldRepository;
+import duclm.repository.TechRepository;
+import duclm.service.UserService;
+import duclm.service.FieldService;
+import duclm.service.JobService;
+import duclm.service.StorageService;
+import duclm.service.TechService;
+
+@Controller
+@RequestMapping("jobs")
+public class JobController {
+	@Autowired
+	JobService jobService;
+	
+	@Autowired
+	UserService userService;
+	
+	@Autowired
+	FieldService fieldService;
+	
+	@Autowired
+	TechService techService;
+	
+	@Autowired
+	HttpSession session;
+	
+	@ModelAttribute("fields")
+	public List<FieldDto> getFields(){
+		return fieldService.findAll().stream().map(item->{
+			FieldDto dto = new FieldDto();
+			BeanUtils.copyProperties(item, dto);
+			return dto;
+		}).toList();
+	}
+	
+	@ModelAttribute("techs")
+	public List<TechDto> getTechs() {
+		return techService.findAll().stream().map(item->{
+			TechDto dto = new TechDto();
+			BeanUtils.copyProperties(item, dto);
+			return dto;
+		}).toList();
+	}
+	
+	@ModelAttribute("scopes")
+	public List<String> getScopes(){
+		return Arrays.asList("Large","Medium","Small");
+	}
+	
+	@ModelAttribute("durations")
+	public List<String> getDurations(){
+		return Arrays.asList("Less than 1 month","1 to 3 months","3 to 6 months","More than 6 months");
+	}
+	
+	@RequestMapping("")
+	public String list(ModelMap model) {
+		List<Job> list = jobService.findAllByStatus(1);
+		
+		model.addAttribute("alljobs", list);
+		
+		return "site/jobs/allJobs";
+	}
+	
+	@GetMapping("detail/{id}")
+	public ModelAndView edit(ModelMap model, @PathVariable("id") Long jobId) {
+		Optional<Job> opt = jobService.findById(jobId);
+		JobDto dto = new JobDto();
+		if(opt.isPresent()) {
+			Job entity = opt.get();
+			
+			BeanUtils.copyProperties(entity, dto);
+			dto.setUserId(entity.getUser().getUserId());
+			
+			model.addAttribute("techList", entity.getTechs().toArray());
+			model.addAttribute("jobField", entity.getField().getName());
+			model.addAttribute("job", dto);
+			
+			return new ModelAndView("site/jobs/detail", model);
+		}
+		
+		model.addAttribute("message", "Job is not exist");
+		return new ModelAndView("forward:/jobs", model);
+	}
+}
---
+++ freelance-platform-demo/src/main/java/duclm/repository/JobRepository.java
@@ -0,0 +1,21 @@
+package duclm.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import duclm.domain.Field;
+import duclm.domain.Job;
+
+@Repository
+public interface JobRepository extends JpaRepository<Job, Long> {
+
+	@Query(value = "SELECT * FROM jobs j WHERE j.user_id = :userid AND j.status!=0", nativeQuery = true)
+	List<Job> findAllJobByUserParam(@Param("userid") Long userid);
+	
+	List<Job> findAllByStatus(int status);
+
+}

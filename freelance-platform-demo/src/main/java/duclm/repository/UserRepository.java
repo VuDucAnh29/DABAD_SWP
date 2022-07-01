a4622de7ed69a5bc3176c9e3bf74dbc4cc6feb46
Le Duc <92800540+leduc212@users.noreply.github.com>
2022-07-01 07:10:42 +0700

    build

---
+++ freelance-platform-demo/src/main/java/duclm/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package duclm.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import duclm.domain.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+	User findByEmail(String email);
+	boolean existsByEmail(String email);
+}

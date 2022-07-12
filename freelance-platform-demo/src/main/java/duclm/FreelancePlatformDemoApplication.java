fc62520a2b585912ddf00fa38c276ab811ec1c76
Le Duc <92800540+leduc212@users.noreply.github.com>
2022-07-12 21:38:27 +0700

    add firebase storage

---
+++ freelance-platform-demo/src/main/java/duclm/FreelancePlatformDemoApplication.java
@@ -0,0 +1,17 @@
+package duclm;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+
+
+@SpringBootApplication
+public class FreelancePlatformDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreelancePlatformDemoApplication.class, args);
+	}
+	
+}

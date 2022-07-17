51a708a87ae29e1b9d1aa0411d0af4369d095569
Le Duc <92800540+leduc212@users.noreply.github.com>
2022-07-17 15:50:36 +0700

    customer + freelancer index

---
+++ freelance-platform-demo/src/main/java/duclm/config/FirebaseProperties.java
@@ -0,0 +1,16 @@
+package duclm.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+import duclm.service.impl.FirebaseImageService;
+import lombok.Data;
+
+@Data
+@Configuration
+@ConfigurationProperties(prefix = "firebase")
+public class FirebaseProperties {
+	private String bucketName;
+
+    private String imageUrl;
+}

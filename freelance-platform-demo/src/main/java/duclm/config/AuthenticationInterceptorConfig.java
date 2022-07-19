908765f188a2c1e95b36305b6d26578cd2d049e6
Le Duc <92800540+leduc212@users.noreply.github.com>
2022-07-19 09:27:00 +0700

    admin func and dashboard

---
+++ freelance-platform-demo/src/main/java/duclm/config/AuthenticationInterceptorConfig.java
@@ -0,0 +1,37 @@
+package duclm.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import duclm.interceptor.AdminAuthenticationInterceptor;
+import duclm.interceptor.CustomerAuthenticationInterceptor;
+import duclm.interceptor.FreelancerAuthenticationInterceptor;
+
+@Configuration
+public class AuthenticationInterceptorConfig implements WebMvcConfigurer {
+
+	@Autowired
+	private CustomerAuthenticationInterceptor customerAuthenticationInterceptor;
+	
+	@Autowired
+	private FreelancerAuthenticationInterceptor freelancerAuthenticationInterceptor;
+	
+	@Autowired
+	private AdminAuthenticationInterceptor adminAuthenticationInterceptor;
+	
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(customerAuthenticationInterceptor)
+		.addPathPatterns("/customer/**");
+		
+		registry.addInterceptor(freelancerAuthenticationInterceptor)
+		.addPathPatterns("/freelancer/**");
+		
+		registry.addInterceptor(adminAuthenticationInterceptor)
+		.addPathPatterns("/admin/**");
+
+	}
+	
+}
